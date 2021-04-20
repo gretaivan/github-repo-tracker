f23bfa2cadce8889d92d3c3f4032ad3c9b7d9045
sem-the-dev <semhar.tesfu@outlook.com>
2021-04-20 14:43:53 +0100

    add babel plugin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
- 
-function App(){
-    return (
-<div>
-    
-</div>
-    )
-} 
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
 export default App;
