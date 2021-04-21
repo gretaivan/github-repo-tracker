0506cb53770ffb6486ab6cf4f2d9689a13e0f2b1
gretaivan <greta.ivan91@gmail.com>
2021-04-21 11:28:28 +0100

    finishes card component

--- src/components/RepoCard/index.js
+++ src/components/RepoCard/index.js
@@ -8,8 +8,8 @@ const RepoCard = ({ repo }) => {
     <div className="card">
       {/* Username: {repo.user} */}
       <h3>Repo: {repo.repo} </h3>
-      <p>Issue Count: {repo.open_issues} </p>
-      <p><span>&#9888;</span>Forks: {repo.forks}</p>
+      <p><span>&#9888;</span>Issue Count: {repo.open_issues} </p>
+      <p><span>&#9903;</span>Forks: {repo.forks}</p>
       <p><span>&#9734;</span>Stargazers: {repo.stargazers}</p>
     </div>
   );
